d1a92d046618022dc4de33fa4476a7db8172be3f
Jeremy Maziarz <jmaziarz@mitre.org>
2014-05-31 12:22:34 -0400

    Remove duplicates and remove non-alphanumeric characters from username

--- ba2untangle.rb
+++ ba2untangle.rb
@@ -7,21 +7,37 @@ require 'digest/sha1'
 require 'json'
 
 untangle_users = { javaClass: "java.util.LinkedList", list: [] }
+usernames = []
 
 CSV.foreach("./data/volunteer-export.csv") do |row|
-  user = { email: row[4], 
-           firstName: row[1], 
-           javaClass: "com.untangle.uvm.LocalDirectoryUser", 
-           lastName: row[2], 
-           passwordBase64Hash: Base64.strict_encode64(row[0]), 
-           passwordMd5Hash: Digest::MD5.hexdigest(row[0]), 
-           passwordShaHash: Digest::SHA1.hexdigest(row[0]), 
-           username: row[3] }
- 
-  # Skip the header row 
-  unless row[3] == 'Username'
-    untangle_users[:list] << user
+  # firstName, lastName, and username can only be alphanumeric (including underscore)
+  password  = row[0]
+  first_name = row[1].gsub(/\W/, '')
+  last_name  = row[2].gsub(/\W/, '')
+  username  = row[3].gsub(/\W/, '')
+
+  # Skip the header row
+  if username == 'Username'
+    next
   end
+
+  # Skip duplicates
+  if usernames.include?(username)
+    next
+  else
+    usernames << username
+  end
+
+  user = { email: row[4], 
+           firstName: first_name, 
+           javaClass: "com.untangle.uvm.LocalDirectoryUser", 
+           lastName: last_name, 
+           passwordBase64Hash: Base64.strict_encode64(password), 
+           passwordMd5Hash: Digest::MD5.hexdigest(password), 
+           passwordShaHash: Digest::SHA1.hexdigest(password), 
+           username: username }
+ 
+  untangle_users[:list] << user
 end
 
 puts JSON.pretty_generate(untangle_users)
